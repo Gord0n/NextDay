0c0cea33dbac75f2c0d5877df3e085237eeb49f7
Gordon <Gord0n@users.noreply.github.com>
2019-11-18 15:47:23 -0500

    Initial Commit

---
+++ NextDay/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using NextDay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NextDay.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        CostarEntities db = new CostarEntities();
+
+        public ActionResult Index()
+        {
+            DateTime today = DateTime.Today;
+            DateTime tempTime = new DateTime(2019, 01, 01);
+
+            Documents docs = db.Documents.Where(o => o.AppointmentDate == tempTime).FirstOrDefault();
+
+            DocumentTypes docTypes = db.DocumentTypes.FirstOrDefault();
+
+
+            return View();
+        }
+
+    }
+}

6bac98e4eb19b2dcfb65627d672e337c35742621
Gordon <Gord0n@users.noreply.github.com>
2019-11-21 12:28:34 -0500

    Some tweaks to displaying the data

--- NextDay/Controllers/HomeController.cs
+++ NextDay/Controllers/HomeController.cs
@@ -19,8 +19,9 @@ namespace NextDay.Controllers
             DateTime tempTime = new DateTime(2019, 01, 01);
             DateTime nextWeek = tempTime.AddDays(7);
 
-            List<Documents> docs = db.Documents.Where(o => o.OrderDate > tempTime && o.OrderDate < nextWeek).OrderBy(o => o.Document).OrderBy(o => o.AppointmentDate).ToList();
-            List<DocumentDetails> docDetails = db.DocumentDetails.Where(o=>(o.ServiceDate > tempTime && o.ServiceDate < nextWeek) && (o.ProductTypeCategory == "TIRES" || o.Item == "TRST")).OrderBy(o=>o.Document).OrderBy(o=>o.ServiceDate).ToList();
+            List<Documents> docs = db.Documents.Where(o => o.OrderDate > tempTime && o.OrderDate < today).OrderBy(o => o.Document).OrderBy(o => o.AppointmentDate).ToList();
+            //List<DocumentDetails> docDetails = db.DocumentDetails.Where(o=>(o.ServiceDate > tempTime && o.ServiceDate < nextWeek) && (o.ProductTypeCategory == "TIRES" || o.Item == "TRST") && (o.DocumentType == "Appointment" || o.DocumentType == "RepairOrder" || o.DocumentType == "PO")).OrderBy(o=>o.Document).OrderBy(o=>o.ServiceDate).ToList();
+            List<DocumentDetails> docDetails = db.DocumentDetails.Where(o => (o.ServiceDate > tempTime && o.ServiceDate < nextWeek) && (o.ProductTypeCategory == "TIRES" || o.Item == "TRST")).OrderBy(o => o.Document).OrderBy(o => o.ServiceDate).ToList();
 
             List<NextDayInformation> content = new List<NextDayInformation>();
             string prevDocument = "";
@@ -28,28 +29,31 @@ namespace NextDay.Controllers
             foreach(DocumentDetails Details in docDetails)
             {
                 //If it equals the previous than its still the same information and so we just add on to DocumentInformation
-                if (prevDocument == Details.Document)
-                {
-                    content.Last().DocumentInformation.Add("Test");
-                }
-                else
+                if (prevDocument != Details.Document)
                 {
                     Documents currentDocument = docs.Where(o => o.Document == Details.Document).FirstOrDefault();
-                    
-                    DateTime apointStartTime = docs.Where(o => o.Document == Details.Document).FirstOrDefault().AppointmentTime ?? new DateTime(2000, 01, 01);
-                    DateTime apointEndTime = docs.Where(o => o.Document == Details.Document).FirstOrDefault().PromisedTime ?? new DateTime(2000, 01, 01);
-                    
+
+                    DateTime apointStartTime = docs.Where(o => o.Document == Details.Document).FirstOrDefault().AppointmentTime ?? DateTime.Now;
+                    DateTime apointEndTime = docs.Where(o => o.Document == Details.Document).FirstOrDefault().PromisedTime ?? DateTime.Now;
+
                     content.Add(new NextDayInformation()
                     {
                         DocumentDate = (DateTime)Details.ServiceDate,
                         DocumentType = "" + Details.DocumentType + " " + Details.Document,
                         DocumentTime = "" + apointStartTime.Hour + " - " + apointEndTime.Hour,
                         CustomerInformation = currentDocument.Name,
-                        CustomerVehicleInformation = currentDocument.VehicleYear + " " + currentDocument.VehicleMake + " " + currentDocument.VehicleModel                        
+                        CustomerVehicleInformation = currentDocument.VehicleYear + " " + currentDocument.VehicleMake + " " + currentDocument.VehicleModel
                     });
-
-                    content.Last().DocumentInformation.Add(Details.Description);
                     
+                }              
+
+                if (Details.ProductTypeCategory == "TIRES")
+                {
+                    content.Last().DocumentInformation.Add(Details.Size + " " + Details.Description + " - " + Details.Item + " (" + Details.OrderQty + ")");
+                }
+                else
+                {
+                    content.Last().DocumentInformation.Add(Details.Description);
                 }
 
                 prevDocument = Details.Document;
